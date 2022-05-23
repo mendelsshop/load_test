8f5de03c273e605d897d2554ea3fceee618a83f0
Mendel Rubin <mendelsshop@gmail.com>
2022-05-23 01:56:16 -0400

    moved into seperete example git repo

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use colored::Color;
+use loading_bar::LoadingBar;
+use std::{io::Write, thread, time};
+
+fn main() {
+    let mut bar = LoadingBar::new(50, Some(Color::Green));
+
+    print!("{}", bar);
+    std::io::stdout().flush().unwrap();
+    // flush stdout or else wont work properly
+
+    for _i in 0..5 {
+        if bar.done {
+            // we dont want to advance the bar if it is done
+            break;
+        }
+        let ten_millis = time::Duration::from_secs(3); // wow what a descriptive name
+        thread::sleep(ten_millis); // sleep for 1 seconds
+        bar.advance_by_percent(25.0); // advancing by 25%
+        print!("{}", bar);
+        bar.change_color(Color::Red);
+        // flush stdout or else wont work properly
+        std::io::stdout().flush().unwrap();
+    }
+    println!("");
+}
