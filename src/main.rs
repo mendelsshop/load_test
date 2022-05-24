dfeac6a8dc1eb6424e182e8c9688404fb1fbcdd9
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-05-24 10:10:40 -0400

    updated per libray no need to print each time it updates

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use loading_bar::{Color, LoadingBar};
-use std::{io::Write, thread, time};
+use std::{
+    io::{self, Write},
+    thread, time,
+};
 
 fn main() {
-    let mut bar = LoadingBar::new(4, Some(Color::Green));
-    // bar.display();
+    let mut bar = LoadingBar::new(8, Some(Color::Green));
 
     // flush stdout or else wont work properly
-
+    print!("\r{}", bar);
+    io::stdout().flush().unwrap();
     for i in 0..9 {
         if bar.done {
             // we dont want to advance the bar if it is done
             return;
         }
-        let ten_millis = time::Duration::from_secs_f32(1.0); // wow what a descriptive name
+        let ten_millis = time::Duration::from_secs_f32(2.0); // wow what a descriptive name
         thread::sleep(ten_millis); // sleep for 1 seconds
 
         // bar.advance_by_percent(600.0); this will panic because were going over a 100%
@@ -35,12 +38,6 @@ fn main() {
                 bar.change_color(None);
             }
         };
-
-        print!("{}", bar);
-        // bar.display();
-
-        // flush stdout or else wont work properly
-        std::io::stdout().flush().unwrap();
     }
     println!();
 }
